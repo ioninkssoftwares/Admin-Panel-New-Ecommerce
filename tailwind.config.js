6d53ae61edb10b5d38c7282b4f546e57bfa2325f
Shubham <khobragade.shubham@gmail.com>
2024-08-10 23:23:40 +0530

    add few libraries

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: [
+  //   "./index.html",
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+  // ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+       'primary-blue': 'orange',
+      },
+    },
+  },
+  // plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms')],
+}
+
